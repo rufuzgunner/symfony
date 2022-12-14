bdbcdc0358aa33002546e6d391121507e674d95f
rufuzgunner <rufuzgunner@yandex.ru>
2022-12-15 01:50:18 +0300

    first page with route and controller - lucky number

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,21 @@
+<?php
+
+// src/Controller/LuckyController.php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class LuckyController {
+    
+    public function number(): Response {
+
+        // Логика
+        $number = random_int(0, 100);
+
+        // Возвращаем данные - вывод
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+}
